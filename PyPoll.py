ec1825da8a30ae71fddd8fdaae701eadc66b48a2
DHedden <dirtdog1126@hotmail.com>
2020-07-28 21:44:24 -0400

    Adding PyPoll.py file and 2 folders

---
+++ PyPoll.py
@@ -0,0 +1,16 @@
+# 1. the total number of votes cast 
+# 2. A complete list of candidates who received votes 
+# 3. The percentage of votes each candidate won 
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote 
+
+
+import csv
+dir(csv)
+
+#Open csv and read file
+with open('GitHub/Election_Analysis/Resources/election_results(1).csv') as election_data:
+
+    print(election_data)
+
+
