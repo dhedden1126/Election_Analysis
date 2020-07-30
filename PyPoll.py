c710d29f3afaecb9f541333ebddcbb28723ce799
DHedden <dirtdog1126@hotmail.com>
2020-07-29 20:06:02 -0400

    updated txt file

--- PyPoll.py
+++ PyPoll.py
@@ -45,22 +45,32 @@ with open(file_to_load) as election_data:
 
         candidate_votes[candidate_name] += 1
 
-for candidate_name in candidate_votes:
+with open(file_to_save,"w")as txt_file:
 
-    votes = candidate_votes[candidate_name]
-    vote_percentage= float(votes)/float(total_votes) * 100
+    election_results = (
+        f"\nElection Results\n"
+        f"-----------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-----------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+    
+    for candidate_name in candidate_votes:
 
-    print(f"{candidate_name} received {votes:{6},} that is {vote_percentage:.1f}% of the vote\n")
+        votes = candidate_votes[candidate_name]
+        vote_percentage= float(votes)/float(total_votes) * 100
 
-    if(votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
+        #print(f"{candidate_name} received {votes:{6},} that is {vote_percentage:.1f}% of the vote\n")
 
-winning_candidate_summary = (
-    f"----------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"----------------\n")
-print(winning_candidate_summary)
+        if(votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"----------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"----------------\n")
+    #print(winning_candidate_summary)
